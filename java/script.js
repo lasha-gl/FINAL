75502fb78b57ce0aacba283bce2afd3158b7c4aa
lasha-gl <gegechkori19@gmail.com>
2021-11-10 12:27:34 +0400

    add 1

---
+++ java/script.js
@@ -0,0 +1,23 @@
+// video
+var video = document.getElementById("Video");
+var btn = document.getElementById("Btn");
+
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
+
+
+
+// burger
+
+let navigation = document.getElementById('navigation');
+let burgericon = document.getElementById('burger');
+burgericon.addEventListener('click',function(){
+  navigation.classList.toggle('active');
+})
